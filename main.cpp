1de427f4583c92d17f6968a445292723c2e43068
ChristianWalch <chr.walch@gmail.com>
2019-05-09 19:38:08 +0200

    changed values of quaders and added printf for getFlaecheninhalt()

--- main.cpp
+++ main.cpp
@@ -6,12 +6,16 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Quader quader1(1, 1, 1);
-    Quader quader2(2, 2, 2);
-    Quader quader3(3, 3, 3);
+    Quader quader1(1, 2, 3);
+    Quader quader2(2, 3, 4);
+    Quader quader3(3, 4, 5);
 
     printf("quader1 Volumen: %lf\n", quader1.getVolumen());
     printf("quader2 Volumen: %lf\n", quader2.getVolumen());
     printf("quader3 Volumen: %lf\n", quader3.getVolumen());
+
+    printf("quader1 Flaeche: %lf\n", quader1.getFlaecheninhalt());
+    printf("quader2 Flaeche: %lf\n", quader2.getFlaecheninhalt());
+    printf("quader3 Flaeche: %lf\n", quader3.getFlaecheninhalt());
     return a.exec();
 }
