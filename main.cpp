2f7654fd2fc03b42c1d1963e85038dabc01253a3
ChristianWalch <chr.walch@gmail.com>
2019-05-14 18:09:20 +0200

    std::cin for "quader" parameters

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <QCoreApplication>
 #include "quader.h"
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Quader quader1(1, 2, 3);
-    Quader quader2(2, 3, 4);
-    Quader quader3(3, 4, 5);
+    for(int i = 0; i<3; i++){
+        double laenge, breite, hoehe;
 
-    printf("quader1 Volumen: %lf\n", quader1.getVolumen());
-    printf("quader2 Volumen: %lf\n", quader2.getVolumen());
-    printf("quader3 Volumen: %lf\n", quader3.getVolumen());
+        std::cout << "Laenge eingeben: ";
+        std::cin >> laenge;
+        std::cout << "Breite eingeben: ";
+        std::cin >> breite;
+        std::cout << "Hoehe eingeben: ";
+        std::cin >> hoehe;
 
-    printf("quader1 Flaeche: %lf\n", quader1.getFlaecheninhalt());
-    printf("quader2 Flaeche: %lf\n", quader2.getFlaecheninhalt());
-    printf("quader3 Flaeche: %lf\n", quader3.getFlaecheninhalt());
-    return a.exec();
+        Quader quader(laenge, breite, hoehe);
+
+        std::cout << "Quader Volumen: " << quader.getVolumen() << std::endl;
+        std::cout << "Quader Flaecheninhalt: " << quader.getFlaecheninhalt() << "\n\n";
+    }
+
+    return 0;
+//    return a.exec();
 }
